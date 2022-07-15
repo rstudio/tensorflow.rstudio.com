079fdeefa2a9175cf14c16f440d63520ddd6af4e
Tomasz Kalinowski <kalinowskit@gmail.com>
2022-07-15 05:16:41 -0400

    fix reference builds

---
+++ _utils/build-refs.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+if (!requireNamespace("ecodown", quietly = TRUE))
+  remotes::install_github("edgararuiz/ecodown")
+
+
+library(glue)
+library(fs)
+
+dir_delete("reference")
+dir_create("reference")
+
+try(ecodown::ecodown_build(verbosity = "verbose"))
+
+dir_ls("reference/") %>%
+  fs::path_file() %>%
+  lapply(function(pkg) {
+    file_move(glue("reference/{pkg}/latest/reference"),
+              glue("reference/_{pkg}"))
+    dir_delete(glue("reference/{pkg}"))
+    file_move(glue("reference/_{pkg}"), glue("reference/{pkg}"))
+  })
