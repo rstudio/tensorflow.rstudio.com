9d4f6d70ab3b1c2badae9e0792ad176aa105c8a8
Tomasz Kalinowski <kalinowskit@gmail.com>
2022-07-19 20:58:46 -0400

    rework references page build script

--- _utils/build-reference.R
+++ _utils/build-reference.R
@@ -8,14 +8,33 @@ library(glue)
 library(fs)
 
 dir_delete("reference")
-dir_create("reference")
 
-try(ecodown::ecodown_build(verbosity = "verbose"))
+cache_dir <- "/tmp/tf-site/ecodown-reference-cache"
 
+if(dir_exists(cache_dir)) {
+  cli::cli_alert("Reusing ecodown cache")
+  fs::dir_copy(cache_dir, "reference")
+} else {
+  dir_create("reference")
+  try(ecodown::ecodown_build(verbosity = "verbose"))
+  fs::dir_copy("reference", cache_dir)
+}
+
+
+descs <- list(
+  tensorflow = "Tensors, Graphs and other primatives",
+  keras = "High level API for deep learning",
+  tfdatasets = "Create efficient and fast data loading pipelines",
+  tfautograph = "Tools for translating R code to Tensorflow Graphs",
+  tfruns = "Track and visualize training runs and experiments",
+  tfhub = "Access pre-trained models ready for fine-tuning and deployment",
+  cloudml = "R interface to Google CloudML"
+)
 
 dir_ls("reference/") |>
   fs::path_file() |>
   lapply(function(pkg) {
+    # browser()
     file_move(glue("reference/{pkg}/latest/reference"),
               glue("reference/_{pkg}"))
     dir_delete(glue("reference/{pkg}"))
@@ -26,5 +45,39 @@ dir_ls("reference/") |>
     index <- sub(glue("/reference/{pkg}/latest/reference/"),
                  glue("/reference/{pkg}/"),
                  index)
+
+
+    index <- c(glue(
+"---
+title: {pkg}
+description: {descs[[pkg]]}
+---\n\n"), index)
+
     writeLines(index, index_pth)
-  }) | invisible()
+  }) |> invisible()
+
+
+writeLines(
+r"----(---
+title: Function reference
+listing:
+  type: grid
+  sort: ""
+  contents:
+    - "tensorflow/index.qmd"
+    - "keras/index.qmd"
+    - "tfdatasets/index.qmd"
+    - "tfautograph/index.qmd"
+    - "tfruns/index.qmd"
+    - "tfhub/index.qmd"
+    - "cloudml/index.qmd"
+---
+
+# R packages
+
+)----", "reference/index.qmd")
+
+
+md_files <- fs::dir_ls("reference", pattern = "\\.md$", recurse = TRUE, type = "file")
+qmd_files <- fs::path_ext_set(md_files, "qmd")
+fs::file_move(md_files, qmd_files)
